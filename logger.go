97ff3cb3bbd96f5dbcaa6f7349b973dc4c237add
Crispin Veall <crispin.veall@forgerock.com>
2020-10-26 14:11:53 +0000

    Initial load

---
+++ logger.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger struct {
+	*zap.SugaredLogger
+}
+
+func New() *Logger {
+	cfg := zap.Config{
+		Encoding:         "json",
+		Level:            zap.NewAtomicLevelAt(zapcore.DebugLevel),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey: "message",
+
+			LevelKey:    "level",
+			EncodeLevel: zapcore.CapitalLevelEncoder,
+
+			TimeKey:    "@timestamp",
+			EncodeTime: zapcore.ISO8601TimeEncoder,
+
+			CallerKey:    "caller",
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	logger, _ := cfg.Build()
+	l := &Logger{SugaredLogger: logger.Sugar()}
+	return l
+}
